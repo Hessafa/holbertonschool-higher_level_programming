3d2b0b6664dcac12faedf689a95f3baef6f45aeb
Hussah ✨ <10685@holbertonstudents.com>
2025-05-07 01:59:09 +0300

    Update 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113:  # Skip ASCII for 'e' (101) and 'q' (113)
-        print("{:c}".format(i), end="")
+print("".join("{:c}".format(i) for i in range(97, 123) if i != 101 and i != 113), end="")
