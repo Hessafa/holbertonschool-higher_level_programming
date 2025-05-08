0581056c7ba7a76798dda77cf2b04773e1031c64
Hussah ✨ <10685@holbertonstudents.com>
2025-05-08 12:39:53 +0300

    Update 6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 = f"Welcome to {str1} {str2}!"
-print(str1)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[107:112] + str[:6])
