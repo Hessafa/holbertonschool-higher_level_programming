307242fdce7dfd89cf12dfd581c533cb05428c84
Hussah ✨ <10685@holbertonstudents.com>
2025-05-17 21:13:59 +0300

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,25 +1,30 @@
->>> __import__('0-add_integer').add_integer(1, 2)
-3
->>> __import__('0-add_integer').add_integer(100, -2)
-98
->>> __import__('0-add_integer').add_integer(2)
-100
->>> __import__('0-add_integer').add_integer(100.3, -2)
-98
->>> __import__('0-add_integer').add_integer(1.5, 2.7)
-3
->>> __import__('0-add_integer').add_integer("Hello", 2)
-Traceback (most recent call last):
-TypeError: a must be an integer
->>> __import__('0-add_integer').add_integer(2, "World")
-Traceback (most recent call last):
-TypeError: b must be an integer
->>> __import__('0-add_integer').add_integer(None)
-Traceback (most recent call last):
-TypeError: a must be an integer
->>> __import__('0-add_integer').add_integer(1e400, 1)
-Traceback (most recent call last):
-OverflowError: cannot convert float infinity to integer
->>> __import__('0-add_integer').add_integer(float('nan'), 1)
-Traceback (most recent call last):
-ValueError: cannot convert float NaN to integer
+#!/usr/bin/python3
+"""
+
+This module provides a function that add two inegers,
+
+It that both arguments are integers or floats, cast floats to integer.
+"""
+
+
+def add_integer(a, b=98):
+
+    """
+    Add two integer.
+
+    Args:
+        a: The first number.
+        bL defaults to 98.
+
+    Returns:
+        int: The sum of the two integer>
+
+    Rasies:
+        TypeError: If a or b is not an integer or float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
