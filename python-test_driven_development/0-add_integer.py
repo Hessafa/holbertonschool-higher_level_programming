37b6a9f90bdbd5eef90a78226fc3c482128d05e0
Hussah ✨ <10685@holbertonstudents.com>
2025-05-17 16:35:25 +0300

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module provides a function to add two integers or floats."""
 
+
 def add_integer(a, b=98):
     """Adds two integers or floats and returns the integer sum.
 
