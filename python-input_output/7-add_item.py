f055d35cdaba7da2d53903a742ca417e7bc223db
Hussah ✨ <10685@holbertonstudents.com>
2025-06-06 02:10:27 +0300

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""this script takes command line argument and saves it to a file"""
-import json
+"""Adding arguments to a list and saving them to a file"""
 from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-with open("add_item.json", "w") as f:
-    save_to_json_file(argv[1:], "add_item.json")
+try:
+    args_list = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    args_list = []
+
+args_list += argv[1:]
+save_to_json_file(args_list, "add_item.json")
