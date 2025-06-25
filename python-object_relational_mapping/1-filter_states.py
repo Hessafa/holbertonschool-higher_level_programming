3e0938df3b0c8e273aacae42f823319657d23fea
Hussah ✨ <10685@holbertonstudents.com>
2025-06-26 01:19:33 +0300

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
 """
-This script that lists all states with a name
-starting with N from the database hbtn_0e_0_usa
+Lists all states with a name starting with 'N' from the database hbtn_0e_0_usa
+Usage: ./1-filter_states.py <mysql username> <mysql password> <database name>
+Results sorted by states.id ascending
 """
 
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=user, passwd=password, db=db_name,
+                         charset="utf8")
 
     cursor = db.cursor()
-    cursor.execute("SELECT `id`, `name` \
-                    FROM `states` \
-                    WHERE `name` LIKE BINARY 'N%' \
-                    ORDER BY `id` ASC")
-    rows = cursor.fetchall()
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    cursor.execute(query)
 
-    for row in rows:
+    for row in cursor.fetchall():
         print(row)
+
+    cursor.close()
+    db.close()
