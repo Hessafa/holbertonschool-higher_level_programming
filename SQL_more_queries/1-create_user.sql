1c1a1a0be2688df113e4fb2982c95a5f7fcbe3f9
Hussah ✨ <10685@holbertonstudents.com>
2025-06-26 00:40:50 +0300

    Create 1-create_user.sql

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create the user if not exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases/tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Apply the changes immediately
+FLUSH PRIVILEGES;
