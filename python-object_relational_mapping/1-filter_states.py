022e0be1180a5158a49ba2af923e2eeae9606f05
Hussah ✨ <10685@holbertonstudents.com>
2025-06-26 01:11:17 +0300

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,44 +1,23 @@
 #!/usr/bin/python3
 """
-This module use mysqldb to connect with database
-It lists all states which begin by N
+Filter states that start with 'N' from the database hbtn_0e_0_usa
 """
-import MySQLdb as ms
+import MySQLdb
 import sys
 
 
-def main():
-    """
-    lists all states which begin by N
-    """
-
-    if len(sys.argv) != 4:
-        print("Incorrecte call to scripts")
-        return 1
-
-    try:
-        db = ms.connect(
-            host="localhost",
-            port=3306,
-            user=sys.argv[1],
-            password=sys.argv[2],
-            database=sys.argv[3]
-        )
-    except ms.MySQLError as e:
-        print("connection failed: {}".format(e))
-        return 1
-
+def filter_states():
+    """Connect to MySQL and list states starting with 'N'"""
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
-
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
-
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
 
 
 if __name__ == "__main__":
-    main()
+    filter_states()
