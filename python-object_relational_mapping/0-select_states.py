1611ba890d2cc2dde757572cf39362e4e67e51c2
Hussah ✨ <10685@holbertonstudents.com>
2025-06-26 01:18:23 +0300

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 """
-This script lists all states from the
-database `hbtn_0e_0_usa`.
+Script that lists all states from the database hbtn_0e_0_usa
+Usage: ./0-select_states.py <mysql username> <mysql password> <database name>
+Results sorted by states.id ascending
 """
 
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to MySQL server on localhost, port 3306
+    db = MySQLdb.connect(host="localhost", port=3306, user=user,
+                         passwd=password, db=db_name, charset="utf8")
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states`")
-    rows = cursor.fetchall()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
+    rows = cursor.fetchall()
     for row in rows:
         print(row)
+
+    cursor.close()
+    db.close()
