393dd047afeca524269b2c49d3e9da046e70d7bf
Hessafa <10685@holbertonstudents.com>
2025-06-28 20:00:43 +0000

    Fix MySQLdb connection using TCP/IP

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,51 +1,29 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument
+Script that lists all states with a specific name from the database.
+Usage: ./2-my_filter_states.py <username> <password> <database> <state_name>
 """
-
 import MySQLdb
 import sys
 
-
-def filter_states_by_input():
-    """
-    Displays all values in the states table where name matches the argument
-    arguments: mysql username, mysql password, database name and state name
-    Results are sorted in ascending order by states.id
-    Uses format to create the SQL query with user input
-    """
-    # Get command line arguments
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3]
+    db_name = sys.argv[3]
     state_name = sys.argv[4]
-    # Connect to MySQL database
+
     db = MySQLdb.connect(
-        host="localhost",
+        host="127.0.0.1",  # <== use TCP/IP instead of socket
         port=3306,
         user=username,
         passwd=password,
-        db=database,
-        charset="utf8"
+        db=db_name
     )
-    # Create cursor object
-    cursor = db.cursor()
-    # Execute SQL query using format to insert user input
-    query = "SELECT * FROM states WHERE name = '{}' ORDER\
-            BY id ASC".format(state_name)
-    cursor.execute(query)
-    # Fetch all results
-    results = cursor.fetchall()
-    # Print results
-    for row in results:
-        print(row)
 
-    # Close cursor and database connection
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    cursor.execute(query)
+    for row in cursor.fetchall():
+        print(row)
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-
-    filter_states_by_input()
