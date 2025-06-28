3051faaf559b320288b5ed9a29c71b4823d086bb
Hessafa <10685@holbertonstudents.com>
2025-06-28 20:31:40 +0000

    update

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""
-Script that lists all states with a specific name from the database.
-Usage: ./2-my_filter_states.py <username> <password> <database> <state_name>
-"""
+"""This script lists all states matching a given name from the database hbtn_0e_0_usa."""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
+    # Check if correct number of args (no validation needed per instructions)
+    user, passwd, db, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
-    db = MySQLdb.connect(
-        host="127.0.0.1",  # <== use TCP/IP instead of socket
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
+    # Connect to the database
+    db_conn = MySQLdb.connect(host="localhost", port=3306,
+                              user=user, passwd=passwd, db=db,
+                              charset="utf8")
+    cursor = db_conn.cursor()
 
-    cursor = db.cursor()
+    # Format query string exactly as requested (use format)
     query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+
     cursor.execute(query)
-    for row in cursor.fetchall():
+    results = cursor.fetchall()
+
+    for row in results:
         print(row)
+
     cursor.close()
-    db.close()
+    db_conn.close()
